7f32d979c0715c75d40d28e4c4bbc0939535a40a
gabrielSanovis <9novis9@gmail.com>
2022-03-23 01:32:04 -0300

    :package: feature orçamento

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/*ANIMAÇÃO DA PÁGINA*/ 
+
+const debounce = function(func, wait, immediate) {
 var hamburguer = document.querySelector(".hamburguer");
 
 hamburguer.addEventListener("click", ()=>
@@ -8,7 +11,6 @@ const target = document.querySelectorAll("[data-anime]")
 const animationClass = 'animate'
 
 
-const debounce = function(func, wait, immediate) {
     let timeout
     return function(...args) {
         const context = this
@@ -22,7 +24,6 @@ const debounce = function(func, wait, immediate) {
         if (callNow) func.apply(context, args)
     }
 }
-
 
 function animeScroll() {
     const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4)
@@ -44,3 +45,30 @@ if(target.length) {
     }, 400))
 }
 
+/*PARTE QUE CALCULA O ORÇAMENTO (É PARTE DO ORÇAMENTO-PAGE) */
+
+document.querySelector("#qtde").addEventListener("change", atualizarPreco)
+document.querySelector("#js").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-sim").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-nao").addEventListener("change", atualizarPreco)
+document.querySelector("#prazo").addEventListener("change", () => {
+    const prazo = document.querySelector("#prazo").value
+    document.querySelector("label[for=prazo]").innerHTML = `Prazo ${prazo} semanas`
+    atualizarPreco()
+})
+function atualizarPreco () {
+    const qtde = document.querySelector("#qtde").value
+    const temJS = document.querySelector("#js").checked
+    const incluiLayout = document.querySelector("#layout-sim").checked
+    const prazo = document.querySelector("#prazo").value
+    let preco = qtde * 100
+    if(temJS) {
+        preco *= 1.1
+    }
+    if(incluiLayout) {
+        preco += 500
+    }
+    let taxaUrgencia = 1 - prazo * 0.1
+    preco *= 1 + taxaUrgencia
+    document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`
+}
